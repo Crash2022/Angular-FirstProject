2b550268a9c1efe3d7157fd9848da97974ccf4e6
Crash2022 <crash555@mail.ru>
2023-01-24 18:55:49 +0500

    fit: loading app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { Test1Component } from "./test1/test1.component";
+import { AppComponent } from './app.component'
+import { Test1Component } from './test1/test1.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, Test1Component],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'first-test1',
+    selector: 'test1-root',
     templateUrl: './test1.component.html',
     styleUrls: ['./test1.component.scss'],
 })
