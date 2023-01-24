49517ce22df5122a8707f5000e7990f327eca2bc
Crash2022 <crash555@mail.ru>
2023-01-24 20:59:54 +0500

    refactor: clear app to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { Test1Component } from './test1/test1.component'
 
 @NgModule({
-    declarations: [AppComponent, Test1Component],
+    declarations: [AppComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/test1/test1.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'test1-root',
-    templateUrl: './test1.component.html',
-    styleUrls: ['./test1.component.scss'],
-})
-export class Test1Component {}
