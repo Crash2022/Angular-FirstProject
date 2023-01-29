6104e5ee5e00d452f1151fd79620c7b0536129b3
Crash2022 <crash555@mail.ru>
2023-01-29 20:10:01 +0500

    refactor: folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { TodosComponent } from './pages/todos/todos.component'
 import { LoginComponent } from './pages/login/login.component'
-import { ParentComponent } from './pages/parent/parent.component'
-import { ChildComponent } from './pages/parent/child/child.component'
-import { CompAComponent } from './pages/components/comp-a/comp-a.component'
-import { CompBComponent } from './pages/components/comp-b/comp-b.component'
-import { RoutingComponent } from './pages/routing/routing.component'
 import { AppRoutingRoutingModule } from './app-routing-routing.module'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
 import { HomeComponent } from './pages/home/home.component'
 import { UsersComponent } from './pages/users/users.component'
 import { ProfileComponent } from './pages/profile/profile.component'
-import { CredentialsInterceptor } from './interceptors/credentials.interceptor'
+import { CredentialsInterceptor } from './interceptors/credentials.interceptor';
+import { NavigationComponent } from './pages/navigation/navigation.component'
 
 @NgModule({
     declarations: [
         AppComponent,
-        ParentComponent,
-        ChildComponent,
-        CompAComponent,
-        CompBComponent,
         TodosComponent,
         LoginComponent,
-        RoutingComponent,
         NotFoundComponent,
         HomeComponent,
         UsersComponent,
         ProfileComponent,
+        NavigationComponent,
     ],
     imports: [
         BrowserModule,
--- src/app/pages/routing/routing.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'first-routing',
-    templateUrl: './routing.component.html',
-    styleUrls: ['./routing.component.scss'],
-})
-export class RoutingComponent {}
