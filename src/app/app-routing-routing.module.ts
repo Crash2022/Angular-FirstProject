78b7a148f0620f1eead12cb02fde873d97ac2d9b
Crash2022 <crash555@mail.ru>
2023-01-29 10:58:12 +0500

    fix: home route

--- src/app/app-routing-routing.module.ts
+++ src/app/app-routing-routing.module.ts
@@ -8,7 +8,7 @@ import { UsersComponent } from './pages/users/users.component'
 import { ProfileComponent } from './pages/profile/profile.component'
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'todos', component: TodosComponent },
     { path: 'users', component: UsersComponent },
