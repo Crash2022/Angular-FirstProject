3f13c99810584eb1f601936698241368c7756f8a
Crash2022 <crash555@mail.ru>
2023-02-01 00:28:32 +0500

    add: todolist filter buttons

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -6,10 +6,11 @@ import { TodosComponent } from './components/todos/todos.component'
 import { TodolistComponent } from './components/todos/todolist/todolist.component'
 import { FormsModule } from '@angular/forms';
 import { TasksComponent } from './components/todos/todolist/tasks/tasks.component';
-import { TaskComponent } from './components/todos/todolist/tasks/task/task.component'
+import { TaskComponent } from './components/todos/todolist/tasks/task/task.component';
+import { TodolistFilterComponent } from './components/todos/todolist/todolist-filter/todolist-filter.component'
 
 @NgModule({
-    declarations: [TodosComponent, TodolistComponent, TasksComponent, TaskComponent],
+    declarations: [TodosComponent, TodolistComponent, TasksComponent, TaskComponent, TodolistFilterComponent],
     imports: [CommonModule, TodosRoutingModule, FormsModule],
 })
 export class TodosModule {}
